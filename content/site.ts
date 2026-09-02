2b8d1424bc3313a6ee7cefa07773c5ac28132676
manmirror <hello@manmirror.nl>
2026-09-02 12:50:06 +0200

    Update track title and embed URL for SoundCloud

--- content/site.ts
+++ content/site.ts
@@ -173,9 +173,9 @@ export const siteContent = {
   // Het eerste veld dat is ingevuld wordt gebruikt.
   tracks: [
     {
-      title: "SoundCloud — Zomerset 2026",
+      title: "Startup Set - Koningsdag Uithoorn aan de Amstel 2025",
       dotColor: "var(--pink)",
-      embedUrl: "",
+      embedUrl: "<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2091525978&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/maartenverhoef" title="Maarten Verhoef" target="_blank" style="color: #cccccc; text-decoration: none;">Maarten Verhoef</a> · <a href="https://soundcloud.com/maartenverhoef/startup-set-koningsdag-uithoorn-aan-de-amstel-2025" title="Startup Set - Koningsdag Uithoorn aan de Amstel 2025" target="_blank" style="color: #cccccc; text-decoration: none;">Startup Set - Koningsdag Uithoorn aan de Amstel 2025</a></div>",
       audioFile: null as string | null,
       externalUrl: null as string | null,
     },
